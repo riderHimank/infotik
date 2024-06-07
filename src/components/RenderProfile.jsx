9bc20fda336485875f712b56105e9b26983d0fbb
Kapil619 <kapilbadokar321@gmail.com>
2024-06-07 12:18:46 +0530

    [BUG FIX]- Fixed the Flatlist issue on the ProfileScreen. Refactored UI to only have the posts being scrollable on the profile page.

--- src/components/RenderProfile.jsx
+++ src/components/RenderProfile.jsx
@@ -1,43 +1,45 @@
-import { View, Text,TouchableOpacity, Image, TouchableWithoutFeedback } from 'react-native'
+import { View, Text, TouchableOpacity, Image, TouchableWithoutFeedback } from 'react-native'
 import React, { useEffect, useState } from 'react'
-import {COLORS} from '../constants/index'
+import { COLORS } from '../constants/index'
 import tw from '../customtwrnc'
 import { ScrollView } from 'react-native-gesture-handler'
 import ProfileHeader from './ProfileHeader'
 import ProfilePostList from './postList'
 import { useDispatch } from 'react-redux'
-const RenderProfile = ({user:puser,change,posts}) => {
-    const [user, setUser] = useState({});
-    const [newchange, setNewChange] = useState(false);
-    const dispatch = useDispatch()
+const RenderProfile = ({ user: puser, change, posts }) => {
+  const [user, setUser] = useState({});
+  const [newchange, setNewChange] = useState(false);
+  const dispatch = useDispatch()
 
-    useEffect(() => {
-        setUser(puser)
-        setNewChange(prev => !prev);
-    },[change])
+  useEffect(() => {
+    setUser(puser)
+    setNewChange(prev => !prev);
+  }, [change])
 
 
-    const handleClose = () => {
-      dispatch({type: "setOpen",open: false});
+  const handleClose = () => {
+    dispatch({ type: "setOpen", open: false });
   }
   return (
     <TouchableWithoutFeedback onPress={handleClose}>
 
-    
-    <View style={tw`bg-[${COLORS.primary}] flex-1`}>
-        <ScrollView>
-          <ProfileHeader user={user} change={newchange}/>
-          <View style={tw.style(`py-2 px-16 flex items-center justify-between flex-row border-b`,{borderBottomWidth: 1,borderColor: 'grey'})}>
-            <TouchableOpacity style={tw`pb-2 border-b-2 border-white px-4`}>
-              <Image source={require('../../assets/tabs.png')} style={{objectFit: 'contain'}}/>
-            </TouchableOpacity>
-            <TouchableOpacity style={tw`pb-2 px-4`}>
-              <Image source={require('../../assets/hideheart.png')} style={{objectFit: 'contain'}}/>
-            </TouchableOpacity>
-          </View>
-          <ProfilePostList posts={posts} />
-      </ScrollView>
-    </View>
+
+      <View style={tw`bg-[${COLORS.primary}] flex-1`}>
+        {/* <ScrollView> */}
+        {/*Keeping the Scrollview arises an error in Flatlist , So removed the Scrollview making only the posts scrollable, keeping the profile information at place.
+        */}
+        <ProfileHeader user={user} change={newchange} />
+        <View style={tw.style(`py-2 px-16 flex items-center justify-between flex-row border-b`, { borderBottomWidth: 1, borderColor: 'grey' })}>
+          <TouchableOpacity style={tw`pb-2 border-b-2 border-white px-4`}>
+            <Image source={require('../../assets/tabs.png')} style={{ objectFit: 'contain' }} />
+          </TouchableOpacity>
+          <TouchableOpacity style={tw`pb-2 px-4`}>
+            <Image source={require('../../assets/hideheart.png')} style={{ objectFit: 'contain' }} />
+          </TouchableOpacity>
+        </View>
+        <ProfilePostList posts={posts} />
+        {/* </ScrollView> */}
+      </View>
     </TouchableWithoutFeedback>
   )
 }
