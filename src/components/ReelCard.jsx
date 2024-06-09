05a81ac61178c202f83fac2598d7fae1b048ea1c
Kapil619 <kapilbadokar321@gmail.com>
2024-06-09 18:33:16 +0530

    Truncated description for the Discovery screen

--- src/components/ReelCard.jsx
+++ src/components/ReelCard.jsx
@@ -4,19 +4,19 @@ import tw from '../customtwrnc'
 import { COLORS } from '../constants'
 import { TouchableOpacity } from 'react-native-gesture-handler'
 
-const ReelCard = ({media,description,onPress}) => {
-    return(
+const ReelCard = ({ media, description, onPress }) => {
+    return (
         <TouchableOpacity onPress={onPress}>
-        <View style={tw`border border-[${COLORS.secondary}] w-[42] h-[55] rounded-md relative flex justify-end`}>
-            <Image
-                style={tw.style({resizeMode: 'cover'},`absolute top-0 left-0 right-0 bottom-0`)}
-                source={{ uri: media[1] }}
-            />
-        
-        <Text style={tw`text-white/80 p-2`}>
-            {description}
-        </Text>
-        </View>
+            <View style={tw`border border-[${COLORS.secondary}] w-[42] h-[55] rounded-md relative flex justify-end`}>
+                <Image
+                    style={tw.style({ resizeMode: 'cover' }, `absolute top-0 left-0 right-0 bottom-0`)}
+                    source={{ uri: media[1] }}
+                />
+
+                <Text numberOfLines={3} style={tw`text-white/80 p-2`}>
+                    {description}
+                </Text>
+            </View>
         </TouchableOpacity>
     )
 }
