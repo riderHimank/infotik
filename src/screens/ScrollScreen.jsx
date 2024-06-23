d6119ae005828a3fd7a71f03267be12a6071b733
Kapil619 <kapilbadokar321@gmail.com>
2024-06-23 13:11:44 +0530

    Fixed logo on comment screen. Adjusted Scroller Screen padding. (Scroller issue present)

--- src/screens/ScrollScreen.jsx
+++ src/screens/ScrollScreen.jsx
@@ -1,4 +1,4 @@
-import { View, Text, Dimensions, FlatList, StyleSheet, Button  } from 'react-native'
+import { View, Text, Dimensions, FlatList, StyleSheet, Button } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { getPost } from '../redux/actions/user'
 import Scroller from '../components/Scroller'
@@ -6,29 +6,29 @@ import { Video, ResizeMode } from 'expo-av';
 import { useDispatch, useSelector } from 'react-redux';
 
 const ScrollScreen = () => {
-  const {posts} = useSelector(store => store.user);
-  const [change,setChange] = useState(false);
+  const { posts } = useSelector(store => store.user);
+  const [change, setChange] = useState(false);
   const dispatch = useDispatch();
-  
+
   useEffect(() => {
     (
-      async function (){
-       dispatch(getPost());
+      async function () {
+        dispatch(getPost());
       }
     )()
-  },[])
+  }, [])
 
 
-  
+
 
 
   useEffect(() => {
     console.log('change')
     setChange(prev => !prev);
-  },[posts])
+  }, [posts])
   return (
     <View>
-       <Scroller posts={posts} change={change} profile={false}/>  
+      <Scroller posts={posts} change={change} profile={false} />
     </View>
   )
 }
